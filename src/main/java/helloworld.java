e0c20ba19f0c89ce6ada2f2738524f927849b075
Echo <111144877+Echo2f13@users.noreply.github.com>
2023-11-21 18:38:28 +0530

    firstcommit

---
+++ src/main/java/helloworld.java
@@ -0,0 +1,8 @@
+package helloworld-21mic0082;
+import java.util.ResourceBundle;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
